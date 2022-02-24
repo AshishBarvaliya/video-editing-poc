028d0881a03cee71b776e64b969da0a52e1eb1be
Ashish Barvaliya <ashish.b@cloudthing.com>
2022-02-24 17:11:55 +0530

    updated the trim feature

--- index.js
+++ index.js
@@ -20,10 +20,11 @@ const app = express();
 const dir = "public";
 const subDirectory = "public/uploads";
 
-if (!fs.existsSync(dir)) {
+if (!fs.existsSync(dir) || !fs.existsSync(subDirectory)) {
   fs.mkdirSync(dir);
 
   fs.mkdirSync(subDirectory);
+  fs.mkdirSync("public/trimuploads");
 }
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -48,6 +49,17 @@ const videoFilter = function (req, file, cb) {
 
 var upload = multer({ storage: storage, fileFilter: videoFilter });
 var uploadAnyfile = multer({ storage: storage });
+var uploadToTrim = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "public/trimuploads");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+  fileFilter: videoFilter,
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -102,17 +114,16 @@ app.post("/merge", upload.array("files", 1000), async (req, res) => {
     });
 });
 
-app.post("/trim", upload.array("files", 1), (req, res) => {
-  if (!req.files) {
-    res.send(400).json({ message: "No files found!" });
+app.post("/trim", uploadToTrim.array("files", 0), (req, res) => {
+  const videos = glob.sync("public/trimuploads/*.mp4");
+  if (videos.length === 0 || !req.body.starttime || !req.body.duration) {
+    res.send(400).json({ message: "No time found!" });
     return;
   }
 
-  const videos = glob.sync("public/uploads/*.mp4");
-
   ffmpeg(videos[0])
-    .setStartTime("00:00:05")
-    .setDuration(5)
+    .setStartTime(req.body.starttime)
+    .setDuration(parseInt(req.body.duration))
     .output(outputFileName)
     .on("end", function (err) {
       if (!err) {
@@ -124,9 +135,6 @@ app.post("/trim", upload.array("files", 1), (req, res) => {
             path.join(__dirname + `/${outputFileName}`),
             function (err) {
               if (err) throw err;
-              req.files.forEach((file) => {
-                fs.unlinkSync(file.path);
-              });
 
               fs.unlinkSync(outputFileName);
             }
@@ -176,7 +184,76 @@ app.post("/addCaptions", uploadAnyfile.array("files", 2), (req, res) => {
     });
 });
 
-app.post("/thumbnail", upload.array("files", 1), (req, res) => {});
+app.post("/allThumbnails", uploadToTrim.array("files", 1), (req, res) => {
+  if (!req.files) {
+    res.send(400).json({ message: "No files found!" });
+    return;
+  }
+  const videos = glob.sync("public/trimuploads/*.mp4");
+
+  ffmpeg(videos[0])
+    .screenshots({
+      timestamps: [
+        "1%",
+        "15%",
+        "25%",
+        "35%",
+        "50%",
+        "60%",
+        "70%",
+        "80%",
+        "90%",
+        "99%",
+      ],
+      filename: "thumbnail-at-%s-seconds.png",
+      folder: "public/thumbs/",
+      size: "320x240",
+    })
+    .on("end", function (err) {
+      if (!err) {
+        console.log("Screenshots taken");
+        const images = glob.sync("public/thumbs/*.png");
+
+        res.status(200).send({ paths: images });
+        // req.files.forEach((file) => {
+        //   fs.unlinkSync(file.path);
+        // });
+        // fs.readdir("public/thumbs", (err, files) => {
+        //   if (err) console.log(err);
+        //   files.forEach((file) => {
+        //     fs.unlinkSync(path.join("public/thumbs", file));
+        //   });
+        // });
+      }
+    });
+});
+
+app.post("/thumbnail", uploadToTrim.array("files", 0), (req, res) => {
+  const videos = glob.sync("public/trimuploads/*.mp4");
+  if (videos.length === 0 || !req.body.time) {
+    res.send(400).json({ message: "No time found!" });
+    return;
+  }
+
+  ffmpeg(videos[0])
+    .screenshots({
+      timestamps: [req.body.time],
+      filename: "thumbnail-at-%s-seconds.png",
+      folder: "public/selectedthumb/",
+      size: "320x240",
+    })
+    .on("end", function (err) {
+      if (!err) {
+        console.log("Screenshot taken");
+        const [hours, minutes, seconds] = req.body.time.split(":");
+        const totalSeconds = +hours * 60 * 60 + +minutes * 60 + +seconds;
+
+        res.send({
+          path: `public/selectedthumb/thumbnail-at-${totalSeconds}-seconds.png`,
+        });
+      }
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`App is listening on Port ${PORT}`);
